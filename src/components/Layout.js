df47503f1bae4bf22217e325a9017f204d5df8f9
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-24 12:53:46 +0530

    change

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -144,7 +144,7 @@ export default function Layout() {
         <Divider />
         <List>
           {list.map((text, index) => (
-            <ListItem key={text.label} disablePadding sx={{ display: 'block' }}>
+            <ListItem to={text.to} component={Navli} key={text.label} disablePadding sx={{ display: 'block' }}>
               <ListItemButton
                 sx={{
                   minHeight: 48,
