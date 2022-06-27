13d1d93cd3cae2ef658422d88fb9fa16997f329e
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-27 17:23:37 +0530

    change

--- src/containers/Medicine.js
+++ src/containers/Medicine.js
@@ -1,73 +1,108 @@
-import React from 'react'; 
+import React from 'react';
+import * as yup from 'yup';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { Form, Formik, useFormik } from 'formik';
 
 function Medicine() {
+
+    let schema = yup.object().shape({
+        name: yup.string().required("Please enter your medicine name."),
+        quantity: yup.number().required("Please enter your medicine quantity."),
+        price: yup.number().required("Please enter your medicine price."),
+        expiry: yup.number().required("Please enter your medicine expiry year.")
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            quantity: '',
+        },
+        validationSchema: schema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+    let { errors, handleBlur, handleSubmit, handleChange, values, touched } = formik;
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
-      setOpen(true);
+        setOpen(true);
     };
-  
+
     const handleClose = () => {
-      setOpen(false);
+        setOpen(false);
     };
     return (
         <>
             <h1>Medicines</h1>
             <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Add Medicines
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add Medicines</DialogTitle>
-        <DialogContent>
-          <TextField
-            margin="dense"
-            name='name'
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            name='quantity'
-            label="Quantity"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            name='price'
-            label="Price"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            name='expiry'
-            label="Expiry"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type='submit' onClick={handleClose}>Submit</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+                <Button variant="outlined" onClick={handleClickOpen}>
+                    Add Medicines
+                </Button>
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Add Medicines</DialogTitle>
+                    <Formik values={formik}>
+                        <Form onSubmit={handleSubmit}>
+                            <DialogContent>
+
+                                <TextField
+                                    margin="dense"
+                                    name='name'
+                                    label="Name"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                {errors.name && touched.name ? <p style={{color:"#1976d2"}}>{errors.name}</p> : null}
+                                <TextField
+                                    margin="dense"
+                                    name='quantity'
+                                    label="Quantity"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                {errors.quantity && touched.quantity ? <p style={{color:"#1976d2"}}>{errors.quantity}</p> : null}
+                                <TextField
+                                    margin="dense"
+                                    name='price'
+                                    label="Price"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                {errors.price && touched.price ? <p style={{color:"#1976d2"}}>{errors.price}</p> : null}
+                                <TextField
+                                    margin="dense"
+                                    name='expiry'
+                                    label="Expiry"
+                                    fullWidth
+                                    variant="standard"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                {errors.expiry && touched.expiry ? <p style={{color:"#1976d2"}}>{errors.expiry}</p> : null}
+
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={handleClose}>Cancel</Button>
+                                <Button type='submit'>Submit</Button>
+                            </DialogActions>
+                        </Form>
+                    </Formik>
+                </Dialog>
+            </div>
         </>
-        
+
     );
 }
 
