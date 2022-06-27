c17c98417bda0f29089f9e7b43736038e885371e
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-27 16:41:53 +0530

    change

--- src/containers/Medicine.js
+++ src/containers/Medicine.js
@@ -1,9 +1,73 @@
 import React from 'react'; 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 function Medicine() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
-        <div>
+        <>
             <h1>Medicines</h1>
-        </div>
+            <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add Medicines
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add Medicines</DialogTitle>
+        <DialogContent>
+          <TextField
+            margin="dense"
+            name='name'
+            label="Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            name='quantity'
+            label="Quantity"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            name='price'
+            label="Price"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            name='expiry'
+            label="Expiry"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type='submit' onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+        </>
+        
     );
 }
 
