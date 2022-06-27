4d2cd2125a399369dfe2b88abccc030483879ce8
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-27 10:57:07 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Route, Switch } from "react-router-dom";
-import Layout from "./components/layout/Layout"; 
+import Layout from "./components/layout/Layout";
 import Medicine from "./containers/Medicine";
 import Patient from "./containers/Patient";
 
 function App() {
-return (
-<Switch>
-<Route>
-<Layout path={"/medicine"} exact component={Medicine}/>
-<Layout path={"/patient"} exact component={Patient}/>
-</Route>
-</Switch>
+  return (
+    <Switch>
+      <Layout>
+        <Route path={"/medicine"} exact component={Medicine} />
+        <Route path={"/patient"} exact component={Patient} />
+      </Layout>
+    </Switch>
   );
 }
 export default App;
