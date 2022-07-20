6a534c133189d0ead143869f9c4b196841dc1233
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-20 17:47:54 +0530

    change

--- src/containers/Counterdata.js
+++ src/containers/Counterdata.js
@@ -4,7 +4,9 @@ import { Increment,Decrement } from '../Redux/actions/counter.action';
 
 function Counterdata(props) {
     const dispatch = useDispatch();
-    const D = useSelector(state => state.counter);
+    const c = useSelector(state => state.counter)
+
+    console.log(c);
 
     const handledecrement = ()=>{
         dispatch(Decrement())
@@ -16,7 +18,7 @@ function Counterdata(props) {
 
     return (
         <>
-        <div><p style={{color:"black",width:"50px",height:"50px",border:"5px solid cornflowerblue",textAlign:"center",borderRadius:10,lineHeight:"45px"}}>{D.counter}</p></div>
+        <div><p style={{color:"black",width:"50px",height:"50px",border:"5px solid cornflowerblue",textAlign:"center",borderRadius:10,lineHeight:"45px"}}>{c.counter}</p></div>
         <div style={{display:"flex",flexWrap:'wrap'}}>
             <button style={{backgroundColor:"#E74C3C",width:"150px",padding:'15px',border:'none',borderRadius:'10px',fontSize:'17px',color:'white'}} onClick ={()=>handledecrement()}>Decrement</button>
             <button style={{backgroundColor:"#2ECC71",width:"150px",padding:'15px',border:'none',borderRadius:'10px',fontSize:'17px',color:'white'}} onClick ={()=>handleincrement()}>Increment</button>
