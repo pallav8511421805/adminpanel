a4326129e23fcb8d3b296133be0dad4eb9622e88
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-08 12:06:16 +0530

    change

--- src/Examples/Usecallbackexample.js
+++ src/Examples/Usecallbackexample.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import * as React from 'react';
+import { useState } from 'react';
 
 function Usecallbackexample(props) {
+    
+    const [theme,settheme] = useState(false)
+    const themstyle = {
+        backgroundColor: theme ? '#fff' : '#000',
+        color:theme ? '#000' :'#fff'
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+        <button style={themstyle} onClick={()=>{settheme(!true)}}>theme</button>
+        </>
     );
 }
 
