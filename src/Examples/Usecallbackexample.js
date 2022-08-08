93e16741c0a28620a4e349864ae399e511039f12
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-08-08 12:58:29 +0530

    change

--- src/Examples/Usecallbackexample.js
+++ src/Examples/Usecallbackexample.js
@@ -4,34 +4,45 @@ import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 
 function Usecallbackexample(props) {
-    
-    const [Theme,setTheme] = useState(false)
+
+    const [Theme, setTheme] = useState(false)
     const themstyle = {
         backgroundColor: Theme ? '#000' : '#fff',
-        color: Theme ? '#fff' :'#000',
-        width:'150px',
-        height:'150px',
-        borderRadius:'15px',
-        margin:'0 0 10px'
+        width: '150px',
+        height: '150px',
+        borderRadius: '15px',
+        margin: '0 0 10px'
     }
     const themstylebtn = {
-        backgroundColor:'#000',
-        color:'#fff', 
-        width:'50px',
-        height:'50px',
-        lineHeight:'0px',
-        textAlign:'center',
-        borderRadius:'100%',
-        margin:'0 0 10px',
-        border:'none'
+        backgroundColor: '#000',
+        color: '#fff',
+        width: '50px',
+        height: '50px',
+        lineHeight: '0px',
+        textAlign: 'center',
+        borderRadius: '100%',
+        margin: '0 0 10px',
+        border: 'none'
     }
+
     const textdata = {
-        text : Theme ? DarkModeOutlinedIcon : LightModeOutlinedIcon
+        text: Theme ? DarkModeOutlinedIcon : LightModeOutlinedIcon
+    }
+    const themeinput = {
+        borderBottom:'2px solid black',
+        borderRight:'none',
+        borderTop:'none',
+        borderLeft:'none',
+        fontSize:'15px'
     }
     return (
         <>
-        <div style={themstyle}></div>
-        <button style={themstylebtn} onClick={() => setTheme(!Theme)}>{<textdata.text/>}</button>
+            <div>
+                <input style={themeinput} placeholder='Please Enter any number' />
+            </div>
+            <div style={themstyle}>
+            </div>
+            <button style={themstylebtn} onClick={() => setTheme(!Theme)}>{<textdata.text />}</button>
         </>
     );
 }
