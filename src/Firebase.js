91e57d217d5c00ad80bc50d58279b1345f1433ea
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-05 12:56:39 +0530

    change

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA7mpQFYWVldJq21d5fg89uLROKJBp9M0Y',
